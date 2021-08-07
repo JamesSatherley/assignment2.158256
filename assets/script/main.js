7a86071857b63495aaa7a0025515e606802d7cf4
James Satherley <72740217+JamesSatherley@users.noreply.github.com>
2021-08-07 12:45:36 +1200

    Designed with filler images
    waiting on comments and results

---
+++ assets/script/main.js
@@ -0,0 +1,29 @@
+/* code is obselete. found a better way */
+
+/*document.getElementById('home_button').addEventListener('click', function() { 
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    });
+});
+
+document.getElementById('about_button').addEventListener('click', function() { 
+    window.scrollTo({
+        top: 100,
+        behavior: 'smooth',
+    });
+});
+
+document.getElementById('cv_button').addEventListener('click', function() { 
+    window.scrollTo({
+        top: 4000,
+        behavior: 'smooth',
+    });
+});
+
+document.getElementById('contact_button').addEventListener('click', function() { 
+    window.scrollTo({
+        top: 4000,
+        behavior: 'smooth',
+    });
+});*/
